d4f340812bb98029826eb751f66cd154932d6964
byeolpyo <byeolpyo@protonmail.com>
2024-02-17 13:44:06 +0100

    change up home for development

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      //home: const MinigameHigherLower(question: 'Which was earlier???',),
+      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MinigameHigherLower(question: 'Which was earlier???',),
     );
   }
 }
