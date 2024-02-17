96bd4dad7dc5e68699943ad7fdd7830cdcab0913
Kacper Akdag-Ochnik <kacperochnik01@gmail.com>
2024-02-17 15:15:38 +0100

    Timeline improvements

--- lib/timeline/timeline_view.dart
+++ lib/timeline/timeline_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:timeline_tile/timeline_tile.dart';
+
+import 'timeline_tile_widget.dart';
+import '../event.dart';
 
 class TimelineView extends StatelessWidget {
-  const TimelineView({Key? key}) : super(key: key);
+  const TimelineView({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -11,55 +13,13 @@ class TimelineView extends StatelessWidget {
         title: const Text('Timeline'),
       ),
       body: ListView.builder(
-        itemCount: 5, // Example item count
+        itemCount: EventPlaceholder.list.length,
         itemBuilder: (context, index) {
-          // Dummy data for dates and titles
-          List<String> dates = [
-            '2022-01-01',
-            '2022-02-01',
-            '2022-03-01',
-            '2022-04-01',
-            '2022-05-01'
-          ];
-          List<String> titles = [
-            'Event 1',
-            'Event 2',
-            'Event 3',
-            'Event 4',
-            'Event 5'
-          ];
-          return TimelineTile(
-              alignment: TimelineAlign.center,
-              isFirst: index == 0,
-              isLast: index == 4,
-              indicatorStyle: const IndicatorStyle(
-                width: 20,
-                color: Colors.blue,
-                padding: EdgeInsets.all(6),
-              ),
-              beforeLineStyle: const LineStyle(
-                color: Colors.blue,
-                thickness: 2,
-              ),
-              startChild: Align(
-                alignment: Alignment.centerRight,
-                child: Text(
-                  dates[index],
-                  style: const TextStyle(fontWeight: FontWeight.bold),
-                ),
-              ),
-              endChild: Align(
-                alignment: Alignment.centerLeft,
-                child: SizedBox(
-                  width: 200, // Fixed width for the card
-                  child: Card(
-                    child: ListTile(
-                      title: Text(titles[index]),
-                      subtitle: const Text('Event details...'),
-                    ),
-                  ),
-                ),
-              ));
+          return TimelineTileWidget(
+            isFirst: index == 0,
+            isLast: index == EventPlaceholder.list.length - 1,
+            event: EventPlaceholder.list[index],
+          );
         },
       ),
     );
