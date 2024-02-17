c10192ab3314338101b8d412fb85f63ef20fafaf
byeolpyo <byeolpyo@protonmail.com>
2024-02-18 00:51:53 +0100

    game mode skeleton

--- lib/minigames/minigame_screens/minigame_higher_lower.dart
+++ lib/minigames/minigame_screens/minigame_higher_lower.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:bitnbuildfront/minigames/minigame_screens/minigame_generic.dart';
 import 'package:bitnbuildfront/minigames/minigame_tiles/minigame_image_text_tile.dart';
+import 'package:bitnbuildfront/minigames/minigame_screens/minigame_trivia.dart';
 
 class MinigameHigherLower extends StatefulWidget {
   const MinigameHigherLower(
@@ -9,11 +10,13 @@ class MinigameHigherLower extends StatefulWidget {
       required this.question,
       required this.answerId,
       required this.currentProgress,
+      required this.currentScore,
       required this.maxProgress});
   final String question;
   final int answerId;
   final int currentProgress;
   final int maxProgress;
+  final int currentScore;
 
   @override
   State<MinigameHigherLower> createState() => _MinigameHigherLower();
@@ -33,6 +36,37 @@ class _MinigameHigherLower extends State<MinigameHigherLower> {
           _answerColor = Colors.red;
         }
         _submitted = true;
+      } else {
+        if (widget.currentProgress < widget.maxProgress) {
+          Navigator.push(
+            context,
+            PageRouteBuilder(
+              transitionsBuilder:
+                  (context, animation, secondaryAnimation, child) {
+                return ScaleTransition(
+                  alignment: Alignment.center,
+                  scale: Tween<double>(begin: 0.99, end: 1).animate(
+                    CurvedAnimation(
+                      parent: animation,
+                      curve: Curves.ease,
+                    ),
+                  ),
+                  child: child,
+                );
+              },
+              pageBuilder: (BuildContext context, Animation<double> animation,
+                  Animation<double> secondaryAnimation) {
+                return MinigameTrivia(
+                  currentProgress: widget.currentProgress + 1,
+                  answerId: 3,
+                  currentScore: _answerColor == Colors.green ? widget.currentScore+1 : widget.currentScore,
+                  maxProgress: 10,
+                  question: 'dopasuj pary',
+                );
+              },
+            ),
+          );
+        }
       }
     });
   }
@@ -42,6 +76,7 @@ class _MinigameHigherLower extends State<MinigameHigherLower> {
     return MinigameGeneric(
         question: widget.question,
         currentProgress: widget.currentProgress,
+        currentScore: widget.currentScore,
         maxProgress: widget.maxProgress,
         minigameContent: Column(
             mainAxisAlignment: MainAxisAlignment.center,
